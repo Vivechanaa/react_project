7f8a3f160a815e3f5e78f7512d9b897a8a99f538
vivechanaa <maanvivechana@gmail.com>
2021-07-06 01:44:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import CC from './cc'
+
+function App() {
+  return (
+    <div className="App">
+          <h1>Hello World</h1>
+          <h2>Hello React</h2>
+          <h1>Debugging of React Application</h1>
+
+          <cc/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/cc.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+class CC extends React.Component {
+    render() {
+        return <h1>Hello World using Class Component</h1>
+    }
+}
+export default CC;
